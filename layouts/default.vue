5a24f7de021de10ed008fe2b0981c0c241754173
Jamison Rea <blogmedude@gmail.com>
2021-02-22 10:46:18 +0800

    inital commit with: - i18n - login page - homepage - register page

---
+++ layouts/default.vue
@@ -0,0 +1,170 @@
+<template>
+  <v-app>
+    <template  v-if="this.$route.path == '/login' || this.$route.path == '/zh/login' ||  this.$route.path == '/zh/register' ||  this.$route.path == '/register'">
+      <v-main>
+        
+        <v-app-bar
+          app
+          color="white"
+          elevation="0"
+          class="py-2">
+          <v-row justify="center" align="center" >
+           <v-col cols="6">
+              <v-row justify="center" align="center" >
+                <v-col cols="6">
+                  <v-img
+                    class="d-flex "
+                    lazy-src="https://bitss.com/assets/img/bitss-logo.jpg"
+                    max-height="50"
+                    max-width="120"
+                    src="https://bitss.com/assets/img/bitss-logo.jpg"
+                  ></v-img>
+                </v-col>
+                <v-col cols="6">
+                    <nuxt-link :to="switchLocalePath('en')" class="mx-4">English</nuxt-link>
+                    <nuxt-link :to="switchLocalePath('zh')" class="mx-4">Chinese</nuxt-link>
+                  
+                </v-col>
+              </v-row>
+           </v-col>
+          </v-row>
+        </v-app-bar>
+        
+        <nuxt />
+      </v-main>
+      
+    </template>
+    <template v-else>
+      <v-app dark>
+        <v-navigation-drawer
+          v-model="drawer"
+          :mini-variant="miniVariant"
+          :clipped="clipped"
+          fixed
+          app
+        >
+          <v-list>
+            <v-list-item
+              v-for="(item, i) in items"
+              :key="i"
+              :to="item.to"
+              router
+              exact
+            >
+              <v-list-item-action>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-item-action>
+              <v-list-item-content>
+                <v-list-item-title v-text="item.title" />
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+        <v-app-bar
+          :clipped-left="clipped"
+          fixed
+          app
+          color="white"
+          elevation="0"
+          class="py-2"
+        >
+            <v-img
+              class="d-flex"
+              lazy-src="https://bitss.com/assets/img/bitss-logo.jpg"
+              max-height="50"
+              max-width="120"
+              src="https://bitss.com/assets/img/bitss-logo.jpg"
+            ></v-img>
+            <nuxt-link :to="switchLocalePath('en')" class="mx-4">English</nuxt-link>
+            <nuxt-link :to="switchLocalePath('zh')" class="mx-4">Chinese</nuxt-link>
+            <!-- <a
+              href="zh"
+              @click="$router.push(localeRoute({
+                name: 'index',
+                params: { locale: 'zh' }
+              }))">Navigate</a> -->
+          
+          <!-- <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+          <v-btn
+            icon
+            @click.stop="miniVariant = !miniVariant"
+          >
+            <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+          </v-btn>
+          <v-btn
+            icon
+            @click.stop="clipped = !clipped"
+          >
+            <v-icon>mdi-application</v-icon>
+          </v-btn>
+          <v-btn
+            icon
+            @click.stop="fixed = !fixed"
+          >
+            <v-icon>mdi-minus</v-icon>
+          </v-btn>
+          <v-toolbar-title v-text="title" />
+          <v-spacer />
+          <v-btn
+            icon
+            @click.stop="rightDrawer = !rightDrawer"
+          >
+            <v-icon>mdi-menu</v-icon>
+          </v-btn> -->
+        </v-app-bar>
+        <v-main>
+          <nuxt />
+        </v-main>
+        <v-navigation-drawer
+          v-model="rightDrawer"
+          :right="right"
+          temporary
+          fixed
+        >
+          <v-list>
+            <v-list-item @click.native="right = !right">
+              <v-list-item-action>
+                <v-icon light>
+                  mdi-repeat
+                </v-icon>
+              </v-list-item-action>
+              <v-list-item-title>Switch drawer (click me)</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+      </v-app>
+    </template>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  head () {
+    return this.$nuxtI18nHead()
+  },
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-main>
+    <v-container fluid >
+        <v-row justify="center" align="center" no-gutters class="mx-4">
+          <v-col cols="12" sm="8" md="6">
+            <h1 class="text-h3 font-weight-black">{{ $t('homepage.banner.bannerTitle') }}</h1>
+            <p class="mt-2 text-h5 font-weight-light  grey--text text--darken-2">{{ $t('homepage.banner.bannerSubTitle') }}</p>
+            <a href="#" class="text-h5" >{{ $t('homepage.banner.bannerLink') }}</a>
+          </v-col>
+          <v-col cols="12" sm="4" md="6">
+            <iframe width="100%" height="390" src="https://www.youtube.com/embed/Otg5SzdoThM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </v-col>
+        </v-row>
+    </v-container>
+    <v-sheet color="primary" class="pt-8 px-8 pb-2">
+      <v-row>
+        <v-col cols="12" sm="4" md="5">
+          <v-text-field
+            outlined
+            background-color="white"
+            :placeholder="$t('homepage.form.textfieldLinkPlaceholder')"></v-text-field>
+        </v-col>
+        <v-col cols="12" sm="4" md="5">
+          <v-text-field
+            outlined
+            background-color="white"
+            :placeholder="$t('homepage.form.textfieldBrandPlaceholder')"></v-text-field>
+        </v-col>
+        <v-col cols="12" sm="4" md="2">
+          <v-btn
+            block
+            depressed
+            x-large
+            color="yellow darken-1">{{$t('homepage.form.btnSubmit')}}</v-btn>
+        </v-col>
+      </v-row>
+    </v-sheet>
+  </v-main>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  }
+}
+</script>
